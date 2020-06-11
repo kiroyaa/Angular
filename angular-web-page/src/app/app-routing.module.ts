dad27a359c619db440a7f7b4ec16c21f34633a0f
Joel Helkala <joel.helkala@gmail.com>
2020-06-11 23:47:42 +0300

    raw updates

--- angular-web-page/src/app/app-routing.module.ts
+++ angular-web-page/src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AboutComponent } from './about/about.component';
+import { CoursesComponent } from './courses/courses.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', 
+    component: MainPageComponent
+  },
+  { path: 'about', 
+    component: AboutComponent 
+  },
+  { path: 'courses', 
+    component: CoursesComponent 
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-web-page/src/app/app.module.ts
+++ angular-web-page/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AboutComponentComponent } from './about-component/about-component.component';
+import { AboutComponent } from './about/about.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    AboutComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
