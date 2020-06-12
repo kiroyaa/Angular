c9197079d260ab46a16e346343157da8d74ee490
HelkalaGit <joel.helkala@gmail.com>
2020-06-12 19:00:45 +0300

    added ngx datatable and prettier

--- angular-web-page/src/app/app.module.ts
+++ angular-web-page/src/app/app.module.ts
@@ -1,25 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { AboutComponentComponent } from './about-component/about-component.component';
-import { AboutComponent } from './about/about.component';
-import { CoursesComponent } from './courses/courses.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { MainPageComponent } from './main-page/main-page.component'
+import { AboutComponent } from './about/about.component'
+import { CoursesComponent } from './courses/courses.component'
+import { MatTableModule } from '@angular/material/table'
+import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatSortModule } from '@angular/material/sort'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { NgxDatatableModule } from '@swimlane/ngx-datatable'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainPageComponent,
-    AboutComponent,
-    CoursesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MainPageComponent,
+        AboutComponent,
+        CoursesComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        BrowserAnimationsModule,
+        NgxDatatableModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- angular-web-page/src/app/courses/courses.component.ts
+++ angular-web-page/src/app/courses/courses.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
-  selector: 'app-courses',
-  templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+    selector: 'app-courses',
+    templateUrl: './courses.component.html',
+    styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+    title = 'angular-datatables'
+    rows = []
+    constructor() {}
 
-  constructor() { }
+    ngOnInit(): void {
+        this.fetch((data) => {
+            this.rows = data
+        })
+    }
 
-  ngOnInit(): void {
-  }
+    fetch(cb) {
+        const req = new XMLHttpRequest()
+        req.open(
+            'GET',
+            `http://swimlane.github.io/ngx-datatable/assets/data/company.json`
+        )
 
+        req.onload = () => {
+            const data = JSON.parse(req.response)
+            cb(data)
+        }
+
+        req.send()
+    }
 }
